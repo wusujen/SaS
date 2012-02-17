fdb5ed5ac1bde59d63aaff4866d6e6f5d0889c31
Christine Wu <wusujen@gmail.com>
2012-02-17 00:40:40 -0500

    Added new Footer class. Cart Activity is inflated within footer to make footer omnipresent. Scan & search button functionality is switched (must reverse).

--- Cart/src/com/christine/cart/CartActivity.java
+++ Cart/src/com/christine/cart/CartActivity.java
@@ -1,13 +1,14 @@
 package com.christine.cart;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.view.ViewGroup;
 
-public class CartActivity extends Activity {
+public class CartActivity extends FooterActivity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        ViewGroup vg = (ViewGroup) findViewById(R.id.data);
+        ViewGroup.inflate(CartActivity.this, R.layout.cart, vg);
     }
 }
